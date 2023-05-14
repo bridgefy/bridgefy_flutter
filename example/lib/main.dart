0524d9355d1aa435253fff558b764b7686eb3193
Rafael Cardenas <rafael@rafaelcr.com>
2023-05-14 12:39:33 -0600

    chore: main dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,36 +16,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> implements BridgefyDelegate {
   String _platformVersion = 'Unknown';
   final _bridgefy = Bridgefy();
+  bool _didStart = false;
+  String _buttonText = 'Start';
+  String? _uuid;
+  String? _lastMessageId;
 
   @override
   void initState() {
     super.initState();
-    // initPlatformState();
-    _bridgefy.initialize(apiKey: "test", delegate: this);
-    _bridgefy.start();
+    _bridgefy.initialize(apiKey: apiKey, delegate: this);
   }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  // Future<void> initPlatformState() async {
-  //   String platformVersion;
-  //   // Platform messages may fail, so we use a try/catch PlatformException.
-  //   // We also handle the message potentially returning null.
-  //   try {
-  //     platformVersion =
-  //         await _bridgefyPlugin.getPlatformVersion() ?? 'Unknown platform version';
-  //   } on PlatformException {
-  //     platformVersion = 'Failed to get platform version.';
-  //   }
-
-  //   // If the widget was removed from the tree while the asynchronous platform
-  //   // message was in flight, we want to discard the reply rather than calling
-  //   // setState to update our non-existent appearance.
-  //   if (!mounted) return;
-
-  //   setState(() {
-  //     _platformVersion = platformVersion;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -55,78 +35,129 @@ class _MyAppState extends State<MyApp> implements BridgefyDelegate {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: [
+              ElevatedButton(onPressed: _toggleStart, child: Text(_buttonText)),
+              const SizedBox(height: 48),
+              ElevatedButton(onPressed: _send, child: const Text("Send")),
+              Text("UUID: $_uuid"),
+              Text("Last sent message ID: $_lastMessageId"),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  void _toggleStart() {
+    if (!_didStart) {
+      _bridgefy.start();
+    } else {
+      _bridgefy.stop();
+    }
+  }
+
+  void _send() async {
+    final lastMessageId = await _bridgefy.send(
+      data: Uint8List.fromList([0, 1, 2, 3]),
+      transmissionMode: BridgefyTransmissionMode(
+        type: BridgefyTransmissionModeType.broadcast,
+        uuid: await _bridgefy.currentUserID,
+      ),
+    );
+    setState(() {
+      _lastMessageId = lastMessageId;
+    });
+  }
+
   @override
   void bridgefyDidConnect({required String userID}) {
-    // TODO: implement bridgefyDidConnect
+    print("bridgefyDidConnect");
   }
 
   @override
   void bridgefyDidDestroySession() {
-    // TODO: implement bridgefyDidDestroySession
+    print("bridgefyDidDestroySession");
   }
 
   @override
   void bridgefyDidDisconnect({required String userID}) {
-    // TODO: implement bridgefyDidDisconnect
+    print("bridgefyDidDisconnect: $userID");
   }
 
   @override
   void bridgefyDidEstablishSecureConnection({required String userID}) {
-    // TODO: implement bridgefyDidEstablishSecureConnection
+    print("bridgefyDidEstablishSecureConnection: $userID");
   }
 
   @override
-  void bridgefyDidFailSendingMessage({required String messageID, required Error error}) {
-    // TODO: implement bridgefyDidFailSendingMessage
+  void bridgefyDidFailSendingMessage({required String messageID, BridgefyError? error}) {
+    print("bridgefyDidFailSendingMessage: $messageID, $error");
   }
 
   @override
   void bridgefyDidFailToDestroySession() {
-    // TODO: implement bridgefyDidFailToDestroySession
+    print("bridgefyDidFailToDestroySession");
   }
 
   @override
-  void bridgefyDidFailToEstablishSecureConnection(
-      {required String userID, required BridgefyError error}) {
-    // TODO: implement bridgefyDidFailToEstablishSecureConnection
+  void bridgefyDidFailToEstablishSecureConnection({
+    required String userID,
+    required BridgefyError error,
+  }) {
+    print("bridgefyDidFailToEstablishSecureConnection: $userID, $error");
   }
 
   @override
   void bridgefyDidFailToStart({required BridgefyError error}) {
-    // TODO: implement bridgefyDidFailToStart
+    print("bridgefyDidFailToStart: $error");
   }
 
   @override
   void bridgefyDidFailToStop({required BridgefyError error}) {
-    // TODO: implement bridgefyDidFailToStop
+    print("bridgefyDidFailToStop: $error");
   }
 
   @override
-  void bridgefyDidReceiveData(
-      {required Uint8List data,
-      required BridgefyTransmissionMode transmissionMode,
-      String? userID}) {
-    // TODO: implement bridgefyDidReceiveData
+  void bridgefyDidReceiveData({
+    required Uint8List data,
+    required String messageId,
+    required BridgefyTransmissionMode transmissionMode,
+  }) {
+    print("bridgefyDidReceiveData: $data, $messageId, $transmissionMode");
   }
 
   @override
   void bridgefyDidSendMessage({required String messageID}) {
-    // TODO: implement bridgefyDidSendMessage
+    print("bridgefyDidSendMessage: $messageID");
   }
 
   @override
   void bridgefyDidStart({required String currentUserID}) {
-    // TODO: implement bridgefyDidStart
+    print("bridgefyDidStart: $currentUserID");
+    setState(() {
+      _didStart = true;
+      _buttonText = 'Stop';
+      _uuid = currentUserID;
+    });
   }
 
   @override
   void bridgefyDidStop() {
-    // TODO: implement bridgefyDidStop
+    print("bridgefyDidStop");
+    setState(() {
+      _didStart = false;
+      _buttonText = 'Start';
+      _uuid = null;
+    });
+  }
+
+  @override
+  void bridgefyDidSendDataProgress({
+    required String messageID,
+    required int position,
+    required int of,
+  }) {
+    print("bridgefyDidSendDataProgress: $messageID, $position, $of");
   }
 }
