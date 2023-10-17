2acd93cf5248dc97d32f57d28377acf71fc2dd0c
Gilberto Julian de la Orta <julian@bridgefy.me>
2023-10-17 17:50:30 -0600

    chore: Singleton pattern of Bridgefy class

--- lib/bridgefy.dart
+++ lib/bridgefy.dart
@@ -238,6 +238,20 @@ mixin BridgefyDelegate {
 
 /// Bridgefy SDK
 class Bridgefy {
+
+  static final Bridgefy _instance = Bridgefy._internal();
+
+  // This named constructor is the "real" constructor
+  // It'll be called exactly once, by the static property assignment above
+  // it's also private, so it can only be called in this class
+  Bridgefy._internal();
+
+  // using a factory is important
+  // because it promises to return _an_ object of this type
+  // but it doesn't promise to make a new one.
+  factory Bridgefy() {
+    return _instance;
+  }
   /// Initialize the SDK
   /// - Parameters:
   ///   - apiKey: API key
